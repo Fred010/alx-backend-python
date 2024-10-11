5bfcfe16bbd304d5baa6afe3f070b3f4f64a36df
Fred010 <fjbaafi18@gmail.com>
2024-10-11 20:21:01 +0000

    Complex types - string and int/float to tuple

---
+++ 0x00-python_variable_annotations/.6-sum_mixed_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Computes the sum of a list of integers and floats.
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Computes the sum of a list of integers and floats.
+
+    Args:
+        mxd_lst (List[Union[int, float]]): A list of integers and floats.
+
+    Returns:
+        float: The sum of the elements in the input list.
+    """
+    return sum(mxd_lst)
