226ca90f7b9232ab50fef9521bccb672a9460a98
Fred010 <fjbaafi18@gmail.com>
2024-10-11 15:21:04 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+"""
+Add two numbers of type float and return float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add 2 floats and return sum of type float
+
+    Args:
+    a: first number of type float
+    b: second number of type float
+
+    Returns:
+    The sum of a and b, of type float
+    """
+    return a + b
