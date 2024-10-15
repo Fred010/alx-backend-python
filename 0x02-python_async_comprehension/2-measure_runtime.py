3b4b9b5130605e00aa9b8d8845f0d36ea2499593
Fred010 <fjbaafi18@gmail.com>
2024-10-15 22:49:58 +0000

    parallel comprehensions update

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,32 +1,18 @@
 #!/usr/bin/env python3
-"""Measures the total runtime of executing
-    async_comprehension four times in parallel.
-"""
-
+'''Run time for four parallel comprehensions.
+'''
 import asyncio
-from typing import List
+import time
+from importlib import import_module as using
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measures the total runtime of executing
-    async_comprehension four times in parallel.
-
-    Returns:
-        float: The total runtime in seconds.
-    """
-
-    # Get the current event loop's time
-    start_time = asyncio.get_event_loop().time()
-
-    # Execute async_comprehension four times in parallel
-    await asyncio.gather(
-        *[async_comprehension() for _ in range(4)]  # noqa: E231
-    )
-
-    # Get the current event loop's time again after completion
-    end_time = asyncio.get_event_loop().time()
-
-    # Return the total runtime
-    return end_time - start_time
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
